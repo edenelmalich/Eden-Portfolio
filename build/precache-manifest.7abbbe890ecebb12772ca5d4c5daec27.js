897b4a504951941b2ac5f8dc6e5b873741288390
eden <eelmalich2@gmail.com>
2019-10-25 16:58:02 +0300

    initial commit

---
+++ build/precache-manifest.7abbbe890ecebb12772ca5d4c5daec27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b469fb7df1c841343cffc77dc73428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d77caa9dc09984738500",
+    "url": "/static/css/main.b6ab55e3.chunk.css"
+  },
+  {
+    "revision": "c73fb188195c81759d53",
+    "url": "/static/js/2.d93c350c.chunk.js"
+  },
+  {
+    "revision": "d77caa9dc09984738500",
+    "url": "/static/js/main.b8e4ba4c.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "5d2c6442cf0c5a9f9008395c94fb2ae1",
+    "url": "/static/media/imagePort.5d2c6442.jpg"
+  },
+  {
+    "revision": "cac126c078c233779ffb93b8e941608b",
+    "url": "/static/media/imagebrowser.cac126c0.png"
+  },
+  {
+    "revision": "40a4c41f87c605a9603eea1b217397ed",
+    "url": "/static/media/maxfit.40a4c41f.png"
+  }
+]);
